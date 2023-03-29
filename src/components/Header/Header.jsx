ae2538659abfcb30e2c4a1562cf8d53bc00a80d8
mehedi9868 <mehedi9868@gmail.com>
2023-03-29 16:35:15 +0600

    add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Hello from header component</h2>
-        </div>
+        <Navbar bg="secondary" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home" className="text-white">ITz MOVIES TIME</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className="text-white">Home</Nav.Link>
+                        <Nav.Link href="#link" className="text-white">Blog</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
